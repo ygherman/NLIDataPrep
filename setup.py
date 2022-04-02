e079797d3cace6a404eabe83ff5aeb65fd05c79a
ygherman <gh.gherman@gmail.com>
2022-04-03 00:20:58 +0300

    init commint

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='NLIDataPrep',
+    version='0.0.1',
+    description='Python Package for data preprocessing and preparation for REI prject metadata ingest',
+    py_modules = [""],
+    package_dir={'':'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "license :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: Os Independent",
+    ],
+
+
+)
