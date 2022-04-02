09db7b5ad582d6a8826e1890b83745d0ef3b4c75
ygherman <gh.gherman@gmail.com>
2022-04-03 01:04:11 +0300

    update README and add requirements.txt

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
 
-with open("README.md", r) as fh:
+with open("README.md", 'r') as fh:
     long_description = fh.read()
 
 setup(
     name='NLIDataPrep',
+    url='https://github.com/ygherman/NLIDataPrep',
+    author="Yael Gherman",
+    author_email="gh.gherman@gmail.com",
     version='0.0.1',
     description='Python Package for data preprocessing and preparation for REI prject metadata ingest',
     py_modules = [""],
@@ -13,7 +16,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
         "license :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
-        "Operating System :: Os Independent",
+        "Operating System :: OS Independent",
     ],
     long_description=long_description,
     long_description_content_type="text/markdown",
